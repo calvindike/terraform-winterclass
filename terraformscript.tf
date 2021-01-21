87691330724a35ff16b09fa35aa5f5051f7541db
Cloud User <ec2-user@ip-10-0-1-183.ec2.internal>
2021-01-21 00:01:24 +0000

    First VPC template

--- terraformscript.tf
+++ terraformscript.tf
@@ -1,2 +1,12 @@
 provider "aws" {
-  version = "
+  region      = "us-east-1"
+}
+
+resource "aws_vpc" "devops_vpc" {
+  cidr_block       = "10.0.0.0/16"
+  instance_tenancy = "default"
+
+  tags = {
+    Name = "devvpc"
+  }
+}
